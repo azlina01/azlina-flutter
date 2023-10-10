893dff1ae231e303780602d03c99cdb487499d43
azlina01 <azlinaisnaeni@gmail.com>
2023-10-11 00:31:06 +0700

    second commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:html' as html;
 
 class Home extends StatelessWidget {
   const Home({
@@ -11,6 +12,20 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Azlina-Flutter'),
         backgroundColor: const Color.fromARGB(255, 237, 46, 110),
+        actions: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                html.window.open('https://github.com/azlina01/azlina-flutter.git', 'new tab');
+              },
+              style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.white)),
+              child: const Text(
+                'Git Hub',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
